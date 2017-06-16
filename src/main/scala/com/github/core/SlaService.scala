9a2a963693150a057ec35183e1dc5250145dba93
nyaroshinskiy <nikolay.yaroshinskiy@webtech.in.ua>
2017-06-16 10:24:08 +0300

    sla trait update to actor

--- src/main/scala/com/github/core/SlaService.scala
+++ src/main/scala/com/github/core/SlaService.scala
@@ -1,8 +1,7 @@
 package com.github.core
 
-import scala.concurrent.{ExecutionContext, Future}
 import com.github.model.Sla
 
 trait SlaService {
-  def getSlaByToken(token: String)(implicit ec: ExecutionContext): Future[Sla]
+  def getSlaByToken(token: String): Sla
 }
